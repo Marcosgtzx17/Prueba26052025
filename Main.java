547f3b9d206ef7b65a8fd535d789e28bdb38310b
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-05-26 17:07:58 -0600

    AAAA

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Futbolista f = new Futbolista(1, "Lionel", "Messi", 36, 10, "Delantero");
+        Futbolista f = new Futbolista(1, "Cristiano", "Ronaldo", 36, 10, "Delantero");
         Entrenador e = new Entrenador(2, "Scaloni", "Lionel", 45, "ARG123");
         Masajista m = new Masajista(3, "Pedro", "Gómez", 50, "Fisioterapia", 15);
 
