ba1486aaa17fce222cf47cb4190bfd4736da283c
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-05-26 18:50:37 -0600

    Se hicierion pruiebas, del funcionamiento del programa.

--- Seleccion.java
+++ Seleccion.java
@@ -2,9 +2,9 @@ public class Seleccion {
     public static void main(String[] args) {
         Persona[] integrantes = new Persona[3];
 
-        integrantes[0] = new Futbolista(1, "Lionel", "Messi", 36, 10, "Delantero");
-        integrantes[1] = new Entrenador(2, "Lionel", "Scaloni", 45, "ARG-1234");
-        integrantes[2] = new Masajista(3, "Pedro", "Gomez", 50, "Fisioterapia", 20);
+        integrantes[0] = new Futbolista(1, "Cristiano", "Aveiro", 40, 7, "Delantero");
+        integrantes[1] = new Entrenador(2, "Neymar", "No se cual xd", 45, "BRAZIL123");
+        integrantes[2] = new Masajista(3, "Pintus", "Gomez", 50, "Fisioterapia", 20);
 
         for (Persona p : integrantes) {
             System.out.println("-----");
